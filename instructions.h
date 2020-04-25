72ea002a57396b543789bd691c2d3aeb460e33f1
Hayden Smith <hsmith307@gatech.edu>
2020-04-25 16:40:41 -0400

    nvm done now

--- instructions.h
+++ instructions.h
@@ -5,11 +5,11 @@
 //
 //	instructions, 256x256@4, 
 //	+ palette 256 entries, not compressed
-//	+ 434 tiles (t|f|p reduced) not compressed
+//	+ 428 tiles (t|f|p reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 32x32 
-//	Total size: 512 + 13888 + 2048 = 16448
+//	Total size: 512 + 13696 + 2048 = 16256
 //
-//	Time-stamp: 2020-04-25, 16:35:32
+//	Time-stamp: 2020-04-25, 16:40:23
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_INSTRUCTIONS_H
 #define GRIT_INSTRUCTIONS_H
 
-#define instructionsTilesLen 13888
-extern const unsigned short instructionsTiles[6944];
+#define instructionsTilesLen 13696
+extern const unsigned short instructionsTiles[6848];
 
 #define instructionsMapLen 2048
 extern const unsigned short instructionsMap[1024];
