a3ea93f4977f5f8b8c48fc9d426e0a5b94d8cd66
Hayden Smith <hsmith307@gatech.edu>
2020-04-29 13:10:48 -0400

    new backgrounds added in usenti, not into code yet

---
+++ planet-background-2.h
@@ -0,0 +1,32 @@
+
+//{{BLOCK(planet_background_2)
+
+//======================================================================
+//
+//	planet_background_2, 512x256@4, 
+//	+ palette 256 entries, not compressed
+//	+ 988 tiles (t|f|p reduced) not compressed
+//	+ regular map (in SBBs), not compressed, 64x32 
+//	Total size: 512 + 31616 + 4096 = 36224
+//
+//	Time-stamp: 2020-04-29, 13:09:15
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_PLANET_BACKGROUND_2_H
+#define GRIT_PLANET_BACKGROUND_2_H
+
+#define planet_background_2TilesLen 31616
+extern const unsigned short planet_background_2Tiles[15808];
+
+#define planet_background_2MapLen 4096
+extern const unsigned short planet_background_2Map[2048];
+
+#define planet_background_2PalLen 512
+extern const unsigned short planet_background_2Pal[256];
+
+#endif // GRIT_PLANET_BACKGROUND_2_H
+
+//}}BLOCK(planet_background_2)
---
+++ planet-background1.h
@@ -0,0 +1,32 @@
+
+//{{BLOCK(planet_background1)
+
+//======================================================================
+//
+//	planet_background1, 512x256@4, 
+//	+ palette 256 entries, not compressed
+//	+ 1137 tiles (t|f|p reduced) not compressed
+//	+ regular map (in SBBs), not compressed, 64x32 
+//	Total size: 512 + 36384 + 4096 = 40992
+//
+//	Time-stamp: 2020-04-29, 13:08:54
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_PLANET_BACKGROUND1_H
+#define GRIT_PLANET_BACKGROUND1_H
+
+#define planet_background1TilesLen 36384
+extern const unsigned short planet_background1Tiles[18192];
+
+#define planet_background1MapLen 4096
+extern const unsigned short planet_background1Map[2048];
+
+#define planet_background1PalLen 512
+extern const unsigned short planet_background1Pal[256];
+
+#endif // GRIT_PLANET_BACKGROUND1_H
+
+//}}BLOCK(planet_background1)
